d818b6ee2257af162114aa7cc45d124248b729e4
WorkingwithColt <mohitechetan2019@gmail.com>
2024-03-17 13:56:34 +0530

    just added extensions

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
-// const authRoutes = require("./routes/authroutes")
-// const userRoutes = require("./routes/Users")
-// const accountRoutes = require('./routes/Account')
-// const fileRoutes = require('./routes/file')
-const indexRouter = require('./routes/index')
+const authRoutes = require("./routes/authroutes.js")
+const userRoutes = require("./routes/Users.js")
+const accountRoutes = require('./routes/Account.js')
+const fileRoutes = require('./routes/file.js')
+const indexRouter = require('./routes/index.js')
 const port = 3000;
 const cors = require("cors")
 const app = express();//middle ware
-// const multer = require('multer')
-// const path = require('path')        
+const multer = require('multer')
+const path = require('path')        
 
 const bodyParser = require('body-parser')//neating and cleaning 
 // origin: 'http://localhost:19006',
@@ -24,27 +24,27 @@ app.use(cors(corsOptions));
 //     res.header('Access-Control-Allow-Headers', 'Content-Type');
 //     next();
 //   });
-require('./db');
-// require('./models/Users');
-// require("./models/ModalDataSchema")
+require('./db.js');
+require('./models/Users.js');
+require("./models/ModalDataSchema.js")
 
-// const requireToken = require('./Middlewares/AuthTokenRequired');
-// const { mongodbMiddleware } = require('./MongoDB/Mongodbmiddleware');
-// app.use(express.json({limit: '100mb'}));
-// app.use(express.urlencoded({limit: '100mb', extended: true, parameterLimit: 50000}));
-// app.use(bodyParser.json())//server se jo data ata hei voh json mei aa jaye so
-// app.use(express.static('public/Images'))
-// app.use(mongodbMiddleware)
+const requireToken = require('./Middlewares/AuthTokenRequired.js');
+const { mongodbMiddleware } = require('./MongoDB/Mongodbmiddleware.js');
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({limit: '100mb', extended: true, parameterLimit: 50000}));
+app.use(bodyParser.json())//server se jo data ata hei voh json mei aa jaye so
+app.use(express.static('public/Images'))
+app.use(mongodbMiddleware)
 app.use("/", indexRouter);
-// app.use("/auth", authRoutes)
-// app.use("/users", userRoutes)
-// app.use("/account", accountRoutes)
-// app.use("/file", fileRoutes)
+app.use("/auth", authRoutes)
+app.use("/users", userRoutes)
+app.use("/account", accountRoutes)
+app.use("/file", fileRoutes)
 
 
-// app.get('/', requireToken, (req, res) => {
-//     res.send(req.user);
-// })
+app.get('/', requireToken, (req, res) => {
+    res.send(req.user);
+})
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
