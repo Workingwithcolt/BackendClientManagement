2e289849ae73599c3e7dae5cdda61f1ffefe74a1
WorkingwithColt <mohitechetan2019@gmail.com>
2024-03-17 17:20:37 +0530

    removed error at connection

--- db.js
+++ db.js
@@ -12,7 +12,7 @@ require('dotenv').config();
 // })
 
     try {
-        mongoose.connect(process.env.MONGO_URL!);
+        mongoose.connect(process.env.MONGO_URL);
         const connection = mongoose.connection;
         connection.on('connected', () => {
             console.log("mongodb is successfullty connected ");
